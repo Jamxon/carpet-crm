6ffab585568a7278bb5585bf4c6644ad7a3fc889
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-01 00:35:05 +0500

    jvnfdkjvkejjbdvjhbrsejk

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/common/config/bootstrap.php';
-require __DIR__ . '/frontend/config/bootstrap.php';
+require __DIR__ . '/api/config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/common/config/main.php',
     require __DIR__ . '/common/config/main-local.php',
-    require __DIR__ . '/frontend/config/main.php',
-    require __DIR__ . '/frontend/config/main-local.php'
+    require __DIR__ . '/api/config/main.php',
+    require __DIR__ . '/api/config/main-local.php'
 );
 
 (new yii\web\Application($config))->run();
