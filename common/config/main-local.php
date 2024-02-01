fe604de348810ccf401bfd59ce00d5ad7edee736
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-01 18:51:35 +0500

    ersvrse

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=x_u_12889_carpet-crm',
+            'dsn' => 'mysql:host=142.132.192.157;dbname=x_u_12889_carpet-crm',
             'username' => 'x_u_12889_jamkhan',
             'password' => '09022005Jam',
             'charset' => 'utf8',
