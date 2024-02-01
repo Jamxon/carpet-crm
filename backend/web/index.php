b081d84cd6ffeb6c1e49aa61048db23b07d66cbe
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-01 18:26:33 +0500

    xwwqecew

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,18 +1,24 @@
 <?php
-
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
-require __DIR__ . '/../../common/config/bootstrap.php';
-require __DIR__ . '/../config/bootstrap.php';
-
-$config = yii\helpers\ArrayHelper::merge(
-    require __DIR__ . '/../../common/config/main.php',
-    require __DIR__ . '/../../common/config/main-local.php',
-    require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/../config/main-local.php'
-);
-
-(new yii\web\Application($config))->run();
+echo "1";
+try {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+    echo "2";
+    require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/yiisoft/yii2/Yii.php';
+    require __DIR__ . '/common/config/bootstrap.php';
+    require __DIR__ . '/api/config/bootstrap.php';
+    echo "3";
+    $config = yii\helpers\ArrayHelper::merge(
+        require __DIR__ . '/common/config/main.php',
+        require __DIR__ . '/common/config/main-local.php',
+        require __DIR__ . '/api/config/main.php',
+        require __DIR__ . '/api/config/main-local.php'
+    );
+    echo "4";
+    (new yii\web\Application($config))->run();
+    echo "5";
+}catch (Exception $e){
+    echo "6";
+    echo $e;
+}
