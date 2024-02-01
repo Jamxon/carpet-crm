bafff5376117736ac5bb6d7b01120dbf83853ae9
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-01 21:55:29 +0500

    xasc

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=142.132.192.157;dbname=x_u_12889_carpet-crm',
+            'username' => 'x_u_12889_jamkhan',
+            'password' => '09022005Jam',
             'charset' => 'utf8',
         ],
         'mailer' => [
