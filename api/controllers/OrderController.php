ec492872f42deb575fbe038ecaaf7f41b45674d0
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-09 10:49:15 +0500

    ds

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -10,6 +10,22 @@ use yii\rest\ActiveController;
 
 class OrderController extends MyController
 {
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['corsFilter' ] = [
+            'class' => \yii\filters\Cors::class,
+//            'cors' => [
+////                'Origin' => ['*'],
+//                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+//                'Access-Control-Request-Headers' => ['*'],
+//                'Access-Control-Allow-Credentials' => true,
+//                'Access-Control-Max-Age' => 86400,
+//                'Access-Control-Expose-Headers' => ['*']
+//            ],
+        ];
+        return $behaviors;
+    }
     public function actionIndex()
     {
         return new ActiveDataProvider([
