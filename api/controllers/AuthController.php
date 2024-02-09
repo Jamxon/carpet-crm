06d172ada3c306e02b082fe500ae6ce40d7115bd
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-09 10:35:15 +0500

    xwqx

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -14,6 +14,14 @@ class AuthController extends Controller
         $behaviors = parent::behaviors();
         $behaviors['corsFilter' ] = [
             'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => ['*']
+            ],
         ];
         return $behaviors;
     }
