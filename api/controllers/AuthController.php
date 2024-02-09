b50877e9c32ed5db918d191e4bb8060da2e80bd9
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-09 10:46:58 +0500

    xwqxqw

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -13,15 +13,15 @@ class AuthController extends Controller
         //cors errors
         $behaviors = parent::behaviors();
         $behaviors['corsFilter' ] = [
-            'class' => \yii\filters\Cors::className(),
-            'cors' => [
-//                'Origin' => ['*'],
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                'Access-Control-Request-Headers' => ['*'],
-                'Access-Control-Allow-Credentials' => true,
-                'Access-Control-Max-Age' => 86400,
-                'Access-Control-Expose-Headers' => ['*']
-            ],
+            'class' => \yii\filters\Cors::class,
+//            'cors' => [
+////                'Origin' => ['*'],
+//                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+//                'Access-Control-Request-Headers' => ['*'],
+//                'Access-Control-Allow-Credentials' => true,
+//                'Access-Control-Max-Age' => 86400,
+//                'Access-Control-Expose-Headers' => ['*']
+//            ],
         ];
         return $behaviors;
     }
