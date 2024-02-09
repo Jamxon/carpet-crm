225a02dde7eab3173bef23b78d5e6e10f2d3a6da
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-09 09:52:48 +0500

    ccedcew

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -29,11 +29,11 @@ class AuthController extends Controller
         return [
             'corsFilter' => [
                 'class' => \yii\filters\Cors::class,
-//                'cors' => [
-//                    'Access-Control-Allow-Origin' => ['*'],
-//                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
-//                    'Access-Control-Request-Headers' => ['*'],
-//                ],
+                'cors' => [
+                    'Access-Control-Allow-Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
+                    'Access-Control-Request-Headers' => ['*'],
+                ],
             ],
         ];
     }
