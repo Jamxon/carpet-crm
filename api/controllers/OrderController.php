b031dd41b6ac9368d6ad8e8f0d30c8c6ee5ff739
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-11 23:58:05 +0500

    cewcw

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -21,7 +21,7 @@ class OrderController extends MyController
                 'class' => \yii\filters\Cors::className(),
                 'cors' => [
                     // restrict access to
-                    'Origin' => ['http://yii.loc','https://darkorr.vercel.app'],
+                    'Origin' => ['*'],
                     // Allow only POST and PUT methods
                     'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                     // Allow only headers 'X-Wsse'
