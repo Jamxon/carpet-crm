8062825cdfc1c4004f4f421bcc36f2e8e4512fd9
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-11 22:00:47 +0500

    xecw

--- common/config/main.php
+++ common/config/main.php
@@ -6,8 +6,14 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'cache' => [
-            'class' => \yii\caching\FileCache::class,
+        // other components...
+        'response' => [
+            'class' => 'yii\web\Response',
+            'on beforeSend' => function ($event) {
+                $response = $event->sender;
+                $response->headers->add('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+                // Add other CORS headers as needed
+            },
         ],
     ],
 ];
