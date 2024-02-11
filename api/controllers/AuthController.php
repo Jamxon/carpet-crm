53960316bcd17df0fc4f76080f6eb480c06fe01c
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-11 21:26:16 +0500

    ecewsd

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -10,10 +10,26 @@ class AuthController extends Controller
 {
     public function behaviors()
     {
-        $behaviors['corsFilter'] = [
-            'class' => CorsBehavior::class,
+        return [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::class,
+                'cors' => [
+                    // restrict access to
+                    'Origin' => ['https://darkorr.vercel.app/'],
+                    // Allow only POST and PUT methods
+                    'Access-Control-Request-Method' => ['POST', 'PUT'],
+                    // Allow only headers 'X-Wsse'
+                    'Access-Control-Request-Headers' => ['X-Wsse'],
+                    // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+                    'Access-Control-Allow-Credentials' => true,
+                    // Allow OPTIONS caching
+                    'Access-Control-Max-Age' => 3600,
+                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+                ],
+
+            ],
         ];
-        return $this->behaviors;
     }
     public function actionLogin()
     {
