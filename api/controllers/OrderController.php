401ddd243edc05dc81b327008deb45edf9a62718
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-11 23:03:00 +0500

    ecwfew

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -11,31 +11,34 @@ class OrderController extends MyController
 {
     public function behaviors()
     {
-//        $behaviors = parent::behaviors();
-//
-//        $behaviors['corsFilter'] = [
-//            'class' => \yii\filters\Cors::className(),
-//        ];
-//
-//        return $behaviors;
-        //cors error react request header field access-control-allow-origin is not allowed by access-control-allow-headers in preflight response
         return [
+            'authenticator' => [
+                'class' => \yii\filters\auth\HttpBearerAuth::class,
+                'except' => ['login', 'logout'],
+            ],
+            'verbs' => [
+                'class' => \yii\filters\VerbFilter::class,
+                'actions' => [
+                    'login' => ['POST'],
+                    'logout' => ['POST'],
+                ],
+            ],
             'corsFilter' => [
                 'class' => \yii\filters\Cors::class,
-                'cors' => [
-                    // restrict access to
-                    'Origin' => ['*'],
-                    // Allow only POST and PUT methods
-                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                    // Allow only headers 'X-Wsse'
-                    'Access-Control-Request-Headers' => ['*'],
-                    // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
-                    'Access-Control-Allow-Credentials' => true,
-                    // Allow OPTIONS caching
-                    'Access-Control-Max-Age' => 3600,
-                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
-                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
-                ],
+//                'cors' => [
+//                    // restrict access to
+//                    'Origin' => ['*'],
+//                    // Allow only POST and PUT methods
+//                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+//                    // Allow only headers 'X-Wsse'
+//                    'Access-Control-Request-Headers' => ['*'],
+//                    // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+//                    'Access-Control-Allow-Credentials' => true,
+//                    // Allow OPTIONS caching
+//                    'Access-Control-Max-Age' => 3600,
+//                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+//                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+//                ],
 
             ],
         ];
