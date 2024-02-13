747d43eb6019fe11bd58e7a07a350edab13d162f
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-13 12:38:17 +0500

    refvef

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -8,40 +8,40 @@ use yii\rest\Controller;
 
 class AuthController extends Controller
 {
-//    public function behaviors()
-//    {
-//        return [
-//            'authenticator' => [
-//                'class' => \yii\filters\auth\HttpBearerAuth::class,
-//                'except' => ['login', 'logout'],
-//            ],
-//            'verbs' => [
-//                'class' => \yii\filters\VerbFilter::class,
-//                'actions' => [
-//                    'login' => ['POST'],
-//                    'logout' => ['POST'],
+    public function behaviors()
+    {
+        return [
+            'authenticator' => [
+                'class' => \yii\filters\auth\HttpBearerAuth::class,
+                'except' => ['login', 'logout'],
+            ],
+            'verbs' => [
+                'class' => \yii\filters\VerbFilter::class,
+                'actions' => [
+                    'login' => ['POST'],
+                    'logout' => ['POST'],
+                ],
+            ],
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::class,
+//                'cors' => [
+//                    // restrict access to
+//                    'Origin' => ['*'],
+//                    // Allow only POST and PUT methods
+//                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+//                    // Allow only headers 'X-Wsse'
+//                    'Access-Control-Request-Headers' => ['*'],
+//                    // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+//                    'Access-Control-Allow-Credentials' => true,
+//                    // Allow OPTIONS caching
+//                    'Access-Control-Max-Age' => 3600,
+//                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+//                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
 //                ],
-//            ],
-//            'corsFilter' => [
-//                'class' => \yii\filters\Cors::class,
-////                'cors' => [
-////                    // restrict access to
-////                    'Origin' => ['*'],
-////                    // Allow only POST and PUT methods
-////                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-////                    // Allow only headers 'X-Wsse'
-////                    'Access-Control-Request-Headers' => ['*'],
-////                    // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
-////                    'Access-Control-Allow-Credentials' => true,
-////                    // Allow OPTIONS caching
-////                    'Access-Control-Max-Age' => 3600,
-////                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
-////                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
-////                ],
-//
-//            ],
-//        ];
-//    }
+
+            ],
+        ];
+    }
     public function actionLogin()
     {
         $model =new LoginForm();
