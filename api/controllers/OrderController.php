482f628187f4b09306ac2412c7e2b7ea4ade4d45
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-14 23:00:18 +0500

    dwerdfg

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -21,16 +21,8 @@ class OrderController extends MyController
                     'application/json' => Response::FORMAT_JSON,
                 ],
             ],
-            'cors' => [
+            'corsFilter' => [
                 'class' => \yii\filters\Cors::class,
-                'cors' => [
-                    'Access-Control-Allow-Origin' => ['http://yii.loc', 'https://darkorr.vercel.app'],
-                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                    'Access-Control-Request-Headers' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
-                    'Access-Control-Max-Age' => 86400,
-                    'Access-Control-Expose-Headers' => ['*'],
-                ],
             ],
         ];
     }
