3d824252db56ec7b3e5b28cf817d64bdc50db404
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-14 22:09:00 +0500

    4ertgf

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -24,7 +24,7 @@ class OrderController extends MyController
             'cors' => [
                 'class' => \yii\filters\Cors::class,
                 'cors' => [
-                    'Origin' => ['http://yii.loc', 'https://darkorr.vercel.app'],
+                    'Origin' => ['*'],
                     'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                     'Access-Control-Request-Headers' => ['*'],
                     'Access-Control-Allow-Credentials' => true,
