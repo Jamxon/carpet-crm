dd41c266521cfff4aeb306fcf5329914830aa78b
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-14 21:58:43 +0500

    xewds

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -20,7 +20,15 @@ class OrderController extends MyController
             ],
             'corsFilter' => [
                 'class' => \yii\filters\Cors::class,
-            ]
+                'cors' => [
+                    'Origin' => ['http://yii.loc'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age' => 86400,
+                    'Access-Control-Expose-Headers' => ['*'],
+                ],
+                ]
         ];
     }
     public function actionIndex()
