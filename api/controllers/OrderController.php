d46509bbffac73fadabb3f23a1ab26fecd72794c
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-14 22:04:14 +0500

    xewsdf

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -24,12 +24,15 @@ class OrderController extends MyController
             'corsFilter' => [
                 'class' => 'yii\filters\Cors',
                 'cors' => [
-                    'Origin' => ['http://yii.loc', 'https://darkorr.vercel.app/operator/mijozlar'],
-                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                    'Access-Control-Request-Headers' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
-                    'Access-Control-Max-Age' => 86400,
-                    'Access-Control-Expose-Headers' => ['*'],
+                    'class' => \yii\filters\Cors::class,
+                    'cors' => [
+                        'Origin' => ['http://yii.loc', 'https://darkorr.vercel.app'],
+                        'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                        'Access-Control-Request-Headers' => ['*'],
+                        'Access-Control-Allow-Credentials' => true,
+                        'Access-Control-Max-Age' => 86400,
+                        'Access-Control-Expose-Headers' => ['*'],
+                    ],
                 ],
             ]
         ];
