15ac3a3a3c45645ab4fcc77611257a0aa7a1b16b
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-14 22:09:34 +0500

    edsf

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -23,14 +23,14 @@ class OrderController extends MyController
             ],
             'cors' => [
                 'class' => \yii\filters\Cors::class,
-                'cors' => [
-                    'Origin' => ['*'],
-                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                    'Access-Control-Request-Headers' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
-                    'Access-Control-Max-Age' => 86400,
-                    'Access-Control-Expose-Headers' => [],
-                ],
+//                'cors' => [
+//                    'Origin' => ['*'],
+//                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+//                    'Access-Control-Request-Headers' => ['*'],
+//                    'Access-Control-Allow-Credentials' => true,
+//                    'Access-Control-Max-Age' => 86400,
+//                    'Access-Control-Expose-Headers' => [],
+//                ],
             ],
         ];
     }
