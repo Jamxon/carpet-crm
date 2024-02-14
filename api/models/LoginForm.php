0a03d3f39fb5f392b52a5602449579acb567ba6a
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-14 08:56:01 +0500

    ewvw

--- api/models/LoginForm.php
+++ api/models/LoginForm.php
@@ -60,25 +60,14 @@ class LoginForm extends Model
         if ($this->validate()) {
             $user = $this->getUser();
             $access_token = Yii::$app->security->generateRandomString(32);
-
-            // Create a new AccessToken instance
             $userAccessToken = new AccessToken();
             $userAccessToken->user_id = $user->id;
             $userAccessToken->access_token = $access_token;
             $userAccessToken->expired_at = date('Y-m-d H:i:s', strtotime('+1 day'));
-
-//            // Save the AccessToken instance
-//            if ($userAccessToken->save()) {
-//                // Update the used_at timestamp
-//                $userAccessToken->used_at = date('Y-m-d H:i:s');
-//                $userAccessToken->save();
-//                // Return token and other necessary information
+            if ($userAccessToken->save()) {
                 $model = ['access_token' => $access_token, 'type_id' => $user->type_id];
                 return $model;
-//            } else {
-//                // Handle saving failure
-//                return null;
-//            }
+            }
         }
         return ["Tog'ri jo'nat krisa"];
     }
