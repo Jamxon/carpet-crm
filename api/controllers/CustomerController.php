56f3b4ef48b14fcdb7c76cd7e32187235ed95684
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-16 10:18:57 +0500

    werewdwef

--- api/controllers/CustomerController.php
+++ api/controllers/CustomerController.php
@@ -40,6 +40,33 @@ class CustomerController extends MyController
             ]
         ]);
     }
+    private function cyrillic_to_latin($cyrillicString)
+    {
+        $cyr = [
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'ғ', 'ҳ', 'Ғ', 'Ҳ', 'қ', 'Қ',
+            'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'ў', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П',
+            'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Ў', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'
+        ];
+        $lat = [
+            'a', 'b', 'v', 'g', 'd', 'e', 'yo', 'j', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'g\'', 'h', 'G\'', 'H', 'q', 'Q',
+            'r', 's', 't', 'u', 'f', 'x', 's', 'ch', 'sh', 'o\'', '', '', '', 'e', 'yu', 'ya',
+            'A', 'B', 'V', 'G', 'D', 'E', 'Yo', 'J', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P',
+            'R', 'S', 'T', 'U', 'F', 'X', 'S', 'Ch', 'Sh', 'O\'', '', '', '', 'E', 'Yu', 'Ya'
+        ];
+        return str_replace($cyr, $lat, $cyrillicString);
+    }
+    public function actionSearchbyname($name)
+    {
+        $name = $this->cyrillic_to_latin($name);
+        return new ActiveDataProvider([
+            'query' => \common\models\Customer::find()
+                ->where(['LIKE', 'name', $name]),
+            'pagination' => [
+                'pageSize' => 10,
+            ]
+        ]);
+    }
     public function actionIndex()
     {
         return new ActiveDataProvider([
