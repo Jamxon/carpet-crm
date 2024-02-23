25a9d0be7c2216f3821dd5643737791064574054
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-23 09:22:06 +0500

    dewdcseec

--- api/controllers/MainController.php
+++ api/controllers/MainController.php
@@ -21,28 +21,26 @@ class MainController extends MyController
     }
     public function actionIndex()
     {
-//        $registered = count(Customer::find()->where(['created_at' => date(\Yii::$app->request->get('date'))])->all());
+        $registered = count(Customer::find()->where(['created_at' => date(\Yii::$app->request->get('date'))])->all());
         $order = count(Order::find()->where(['created_at' => \Yii::$app->request->get('date')])->all());
         $bringing = count(Order::find()->where(['created_at' => \Yii::$app->request->get('date'),'status' => 'Olib kelishda'])->all());
         $cancelled = count(Order::find()->where(['created_at' => \Yii::$app->request->get('date'), 'status' => 'Bekor qilindi' ])->all());
-//        $register = count(Customer::find()->where(['created_at' => date(\Yii::$app->request->get('date'))])->all());
+        $register = count(Customer::find()->where(['created_at' => date(\Yii::$app->request->get('date'))])->all());
         //olingan orderlar
-//        $registered_order = OrderItem::find()->join('LEFT JOIN', 'order', 'order.id = order_item.order_id')->where(['order.created_at' => date(\Yii::$app->request->get('date'))])->all();
+        $registered_order = OrderItem::find()->join('LEFT JOIN', 'order', 'order.id = order_item.order_id')->where(['order.created_at' => date(\Yii::$app->request->get('date'))])->all();
         $cleaned = count(Order::find()->where(['created_at' => \Yii::$app->request->get('date'), 'status' => 'Quritishda'])->all());
         $packaged = count(Order::find()->where(['created_at' => \Yii::$app->request->get('date'), 'status' => 'Yetkazib berishda'])->all());
         $completed = count(Order::find()->where(['created_at' => \Yii::$app->request->get('date'), 'status' => 'Yakunlandi'])->all());
-        $date = \Yii::$app->request->get('date');
         return [
-//            'registered' => $registered,
+            'registered' => $registered,
             'order' => $order,
             'bringing' => $bringing,
             'cancelled' => $cancelled,
-//            'register' => $register,
+            'register' => $register,
             'cleaned' => $cleaned,
             'packaged' => $packaged,
             'completed' => $completed,
-//            'registered_order' => $registered_order,
-        'date' => $date
+            'registered_order' => $registered_order,
         ];
     }
 }
