e9a83fd135f97d9f3383d49f57490f04a755f43f
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-23 16:15:19 +0500

    rtbtr

--- api/controllers/MainController.php
+++ api/controllers/MainController.php
@@ -28,7 +28,7 @@ class MainController extends MyController
         $bringing = Order::find()->where(['created_at' => \Yii::$app->request->get('date'),'status' => 'Olib kelishda'])->count();
         $cancelled = Order::find()->where(['created_at' => \Yii::$app->request->get('date'), 'status' => 'Bekor qilindi' ])->count();
         $registered_order_item = OrderItem::find()
-            ->select('cleanitem.name, order.status, size, count')
+            ->select('cleanitem.name, order.status, orderitem.size, orderitem.count')
             ->leftJoin('cleanitem', 'cleanitem.id = order_item.cleanitem_id')
             ->leftJoin('order', 'order.id = order_item.order_id')
             ->where(['created_at' => \Yii::$app->request->get('date')])
