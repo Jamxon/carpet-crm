c183bbbafa7b14f6ce1235fc862a4e8827dfeffc
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-28 11:27:26 +0500

    fv

--- api/controllers/AttendanceController.php
+++ api/controllers/AttendanceController.php
@@ -30,38 +30,36 @@ class AttendanceController extends MyController
     }
     public function actionDate(): ActiveDataProvider
     {
-        $start_date = \Yii::$app->request->post('start_date');
-        return $start_date;
-//        if (\Yii::$app->request->post('start_date') == '' && \Yii::$app->request->post('end_date') == ''){
-//            return new ActiveDataProvider([
-//                'query' => Attendance::find(),
-//                'pagination' => [
-//                    'pageSize' => 10,
-//                ]
-//            ]);
-//        } elseif (\Yii::$app->request->post('start_date') == '' && \Yii::$app->request->post('end_date') != ''){
-//            return new ActiveDataProvider([
-//                'query' => Attendance::find()->where(['<=', 'go_time', \Yii::$app->request->post('end_date')]),
-//                'pagination' => [
-//                    'pageSize' => 10,
-//                ],
-//            ]);
-//        } elseif (\Yii::$app->request->post('start_date') != '' && \Yii::$app->request->post('end_date') == ''){
-//            return new ActiveDataProvider([
-//                'query' => Attendance::find()->where(['>=', 'come_time', \Yii::$app->request->post('start_date')]),
-//                'pagination' => [
-//                    'pageSize' => 10,
-//                ],
-//            ]);
-//        } else {
-//            return new ActiveDataProvider([
-//                'query' => Attendance::find()->where(['>=', 'come_time', \Yii::$app->request->post('start_date')])
-//                    ->andWhere(['<=', 'go_time', \Yii::$app->request->post('end_date')]),
-//                'pagination' => [
-//                    'pageSize' => 10,
-//                ],
-//            ]);
-//        }
+        if (\Yii::$app->request->post('start_date') == '' && \Yii::$app->request->post('end_date') == ''){
+            return new ActiveDataProvider([
+                'query' => Attendance::find(),
+                'pagination' => [
+                    'pageSize' => 10,
+                ]
+            ]);
+        } elseif (\Yii::$app->request->post('start_date') == '' && \Yii::$app->request->post('end_date') != ''){
+            return new ActiveDataProvider([
+                'query' => Attendance::find()->where(['<=', 'go_time', \Yii::$app->request->post('end_date')]),
+                'pagination' => [
+                    'pageSize' => 10,
+                ],
+            ]);
+        } elseif (\Yii::$app->request->post('start_date') != '' && \Yii::$app->request->post('end_date') == ''){
+            return new ActiveDataProvider([
+                'query' => Attendance::find()->where(['>=', 'come_time', \Yii::$app->request->post('start_date')]),
+                'pagination' => [
+                    'pageSize' => 10,
+                ],
+            ]);
+        } else {
+            return new ActiveDataProvider([
+                'query' => Attendance::find()->where(['>=', 'come_time', \Yii::$app->request->post('start_date')])
+                    ->andWhere(['<=', 'go_time', \Yii::$app->request->post('end_date')]),
+                'pagination' => [
+                    'pageSize' => 10,
+                ],
+            ]);
+        }
     }
 
     public function actionView($id, $start_date, $end_date)
