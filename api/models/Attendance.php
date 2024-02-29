0e367ecfa1c7ea60389a1a07c7a9481f55cd0741
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-29 22:20:41 +0500

    cdsf dv

--- api/models/Attendance.php
+++ api/models/Attendance.php
@@ -13,6 +13,7 @@ use Yii;
  * @property string|null $go_time
  * @property int|null $full_time
  * @property int|null $daily_salary
+ * @property int|null $status
  * @property string|null $comment
  */
 class Attendance extends \common\models\Attendance
@@ -33,6 +34,7 @@ class Attendance extends \common\models\Attendance
             'go_time',
             'full_time',
             'daily_salary',
+            'status',
             'comment',
         ];
     }
