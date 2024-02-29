a27e9f5c29547110126c4d713f9476260401f725
Jamxon <aliyevjamkhan499@gmail.com>
2024-02-29 23:02:17 +0500

    efrfbgh

--- api/controllers/AttendanceController.php
+++ api/controllers/AttendanceController.php
@@ -28,26 +28,31 @@ class AttendanceController extends MyController
 
     public function actionDate()
     {
-        $startDate = \Yii::$app->request->post('start_date');
-        $endDate = \Yii::$app->request->post('end_date');
+        if (\Yii::$app->request->isPost) {
+            $startDate = \Yii::$app->request->post('start_date');
+            $endDate = \Yii::$app->request->post('end_date');
 
-        $query = Attendance::find();
+            $query = Attendance::find();
 
-        if (!empty($startDate)) {
-            $query->andWhere(['>=', 'come_time', $startDate]);
+            if (!empty($startDate)) {
+                $query->andWhere(['>=', 'come_time', $startDate]);
+            }
+
+            if (!empty($endDate)) {
+                $query->andWhere(['<=', 'go_time', $endDate]);
+            }
+
+            return new ActiveDataProvider([
+                'query' => $query,
+                'pagination' => [
+                    'pageSize' => 10,
+                ],
+            ]);
+        } else {
+            throw new \yii\web\MethodNotAllowedHttpException("Method Not Allowed. This endpoint only supports POST requests.");
         }
-
-        if (!empty($endDate)) {
-            $query->andWhere(['<=', 'go_time', $endDate]);
-        }
-
-        return new ActiveDataProvider([
-            'query' => $query,
-            'pagination' => [
-                'pageSize' => 10,
-            ],
-        ]);
     }
+
 
 
 }
