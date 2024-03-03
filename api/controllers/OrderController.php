de9a36d0d399e44dc16275a623f89f2091fdc506
Jamxon <aliyevjamkhan499@gmail.com>
2024-03-03 21:59:42 +0500

    123

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -21,14 +21,6 @@ class OrderController extends MyController
         $behaviors['contentNegotiator']['formats']['application/json'] = Response::FORMAT_JSON;
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::class,
-            'cors' => [
-                'Origin' => ['*'],
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-                'Access-Control-Request-Headers' => ['*'],
-                'Access-Control-Allow-Credentials' => true,
-                'Access-Control-Max-Age' => 86400,
-                'Access-Control-Expose-Headers' => ['X-Pagination-Per-Page'],
-            ],
         ];
         return $behaviors;
     }
