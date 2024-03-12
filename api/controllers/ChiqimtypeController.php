c01e8071f61681bdf824ba9063c0c5269ed566b7
Jamxon <aliyevjamkhan499@gmail.com>
2024-03-12 14:21:36 +0500

    ervertber

--- api/controllers/ChiqimtypeController.php
+++ api/controllers/ChiqimtypeController.php
@@ -6,7 +6,7 @@ use yii\data\ActiveDataProvider;
 
 class ChiqimtypeController extends MyController
 {
-    public function actionIndex()
+    public function actionIndex(): ActiveDataProvider
     {
         return new ActiveDataProvider([
             'query' => \common\models\Chiqimtype::find(),
@@ -36,11 +36,11 @@ class ChiqimtypeController extends MyController
         return 'id is required';
     }
 
-    public function actionView($id)
+    public function actionView($id): ?\common\models\Chiqimtype
     {
         return \common\models\Chiqimtype::findOne($id);
     }
-    public function actionDelete($id)
+    public function actionDelete($id): int
     {
         return \common\models\Chiqimtype::deleteAll(['id' => $id]);
     }
