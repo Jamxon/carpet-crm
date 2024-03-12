b77ea05747f5f99a32a326f5d13870bd99b8d319
Jamxon <aliyevjamkhan499@gmail.com>
2024-03-12 12:50:53 +0500

    ervertber

---
+++ api/controllers/ChiqimtypeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace api\controllers;
+
+use yii\data\ActiveDataProvider;
+
+class ChiqimtypeController extends MyController
+{
+    public function actionIndex()
+    {
+        return new ActiveDataProvider([
+            'query' => \common\models\Chiqimtype::find(),
+        ]);
+    }
+
+    //method is create update post
+    public function actionCreate()
+    {
+        $model = new \common\models\Chiqimtype();
+        $model->load(\Yii::$app->request->post(), '');
+        if ($model->save()) {
+            return $model;
+        }
+        return $model->getErrors();
+    }
+
+    public function actionUpdate()
+    {
+        if (\Yii::$app->request->post('id')) {
+            $model = \common\models\Chiqimtype::findOne(\Yii::$app->request->post('id'));
+            $model->load(\Yii::$app->request->post(), '');
+            if ($model->save()) {
+                return $model;
+            }
+            return $model->getErrors();
+        }
+        return 'id is required';
+    }
+
+    public function actionView($id)
+    {
+        return \common\models\Chiqimtype::findOne($id);
+    }
+    public function actionDelete($id)
+    {
+        return \common\models\Chiqimtype::deleteAll(['id' => $id]);
+    }
+}
