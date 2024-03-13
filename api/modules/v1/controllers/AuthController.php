45e0e45906b3b5295da86d4833601300f4e1bb56
Jamxon <aliyevjamkhan499@gmail.com>
2024-03-13 21:44:20 +0500

    ervertber

--- api/modules/v1/controllers/AuthController.php
+++ api/modules/v1/controllers/AuthController.php
@@ -11,9 +11,17 @@ class AuthController extends Controller
 {
     public function behaviors()
     {
-        return  [
+        return [
             'authenticator' => [
                 'class' => \yii\filters\auth\HttpBearerAuth::class,
+                'except' => ['login', 'logout'],
+            ],
+            'verbs' => [
+                'class' => \yii\filters\VerbFilter::class,
+                'actions' => [
+                    'login' => ['POST'],
+                    'logout' => ['POST'],
+                ],
             ],
             'corsFilter' => [
                 'class' => \yii\filters\Cors::class,
