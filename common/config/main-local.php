eeef84b58538a91052dc697b24c247a78e12df42
Jamkhan <aliyevjamkhan499@gmail.com>
2024-04-17 15:52:38 +0500

    sedr

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=142.132.192.157;dbname=x_u_12889_carpet-crm',
-            'username' => 'x_u_12889_jamkhan',
-            'password' => '09022005Jam',
+            'dsn' => 'mysql:host=142.132.192.157;dbname=x_u_13864_jamkhan',
+            'username' => 'x_u_13864_jamkhan',
+            'password' => '09022005',
             'charset' => 'utf8',
         ],
         'mailer' => [
