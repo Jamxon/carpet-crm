d0437092266eaeded6b742729bfcd66714f37a7b
Jamkhan <aliyevjamkhan499@gmail.com>
2024-04-27 12:19:32 +0500

    wdesvffbv

--- api/controllers/CustomerController.php
+++ api/controllers/CustomerController.php
@@ -132,8 +132,8 @@ class CustomerController extends Controller
     {
         return new ActiveDataProvider([
             'query' => \common\models\Customer::find()
-                ->where(['LIKE', 'phone_1', $phone])
-                ->orWhere(['LIKE', 'phone_2', $phone]),
+                ->where(['phone_1', $phone])
+                ->orWhere(['phone_2', $phone]),
             'pagination' => [
                 'pageSize' => 10,
             ]
